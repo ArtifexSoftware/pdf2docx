7dbd57627b86a8fc3f856fce3b5d7f56c2b968b8
dothinking <13885442+dothinking@users.noreply.github.com>
2019-06-28 18:33:36 +0800

    Update util.py

--- util.py
+++ util.py
@@ -11,3 +11,36 @@ def getColor(i):
 def rectangle(bbox, linewidth=0.5, linecolor='r', fillcolor='none'):
 	x0, y0, x1, y1 = bbox
 	return patches.Rectangle((x0, y0), x1-x0, y1-y0, linewidth=linewidth, edgecolor=linecolor, facecolor=fillcolor)
+
+def is_end_sentence(text):
+	'''simple rule to check the completence of text
+	   - sentence delimiter at the end of a sentence
+	'''
+	text = text.strip()
+	return text[-1].endswith(('.', '?', '!', ':'))
+
+def is_start_sentence(text):
+	text = text.strip()
+	if not text:
+		return True
+	else:
+		return not text[0].islower() # conservative
+
+def is_vertical_aligned(bbox1, bbox2, horizontal=True):
+	'''check whether two boxes have intersection in vertical direction.
+	   vertical direction is perpendicular to reading direction
+	'''
+	if horizontal: # reading direction: x
+		L1 = bbox1[2]-bbox1[0]
+		L2 = bbox2[2]-bbox2[0]
+		L = max(bbox1[2], bbox2[2]) - min(bbox1[0], bbox2[0])
+	else:
+		L1 = bbox1[3]-bbox1[1]
+		L2 = bbox2[3]-bbox2[1]
+		L = max(bbox1[3], bbox2[3]) - min(bbox1[1], bbox2[1])
+
+	return L1+L2 > L
+
+def is_horizontal_aligned(bbox1, bbox2, horizontal=True):
+	'''it is opposite to vertical align situation'''
+	return is_vertical_aligned(bbox1, bbox2, not horizontal)
