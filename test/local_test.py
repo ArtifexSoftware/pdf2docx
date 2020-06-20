203f63d1c29993b8a25b03e53932ab48616aade4
dothinking <train8808@gmail.com>
2020-06-21 01:57:42 +0800

    update parsing table sample: both explicit and implicit tables

--- test/local_test.py
+++ test/local_test.py
@@ -14,7 +14,7 @@ import json
 if __name__ == '__main__':
 
     output = os.path.join(os.path.dirname(script_path), 'samples')
-    filename = 'demo-table-no-borders'
+    filename = 'demo-table'
     pdf_file = os.path.join(output, f'{filename}.pdf')
     docx_file = os.path.join(output, f'{filename}.docx')
 
