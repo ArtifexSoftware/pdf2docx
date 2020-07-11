d2277dc3d67787b9f5134b169dc07932b2e679d4
dothinking <train8808@gmail.com>
2020-07-12 00:53:33 +0800

    simplify local test file

--- test/local_test.py
+++ test/local_test.py
@@ -1,39 +1,23 @@
+''' local test file for developing, with pdf2docx installed by
+    `python setpy.py develop`
+'''
+
 import os
-import sys
-
-script_path = os.path.abspath(__file__) # current script path
-project_path = os.path.dirname(os.path.dirname(script_path))
-sys.path.append(project_path)
-
-from pdf2docx.reader import Reader
-from pdf2docx.writer import Writer
-
+from pdf2docx.main import parse, extract_tables
 
 
 if __name__ == '__main__':
 
+    script_path = os.path.abspath(__file__) # current script path
     output = os.path.join(os.path.dirname(script_path), 'samples')
     filename = 'demo-table'
     pdf_file = os.path.join(output, f'{filename}.pdf')
     docx_file = os.path.join(output, f'{filename}.docx')
 
-    pdf = Reader(pdf_file, debug=True) # debug mode to plot layout
-    docx = Writer()
+    # convert pdf to docx
+    parse(pdf_file, docx_file, start=0, end=1)
 
-    for page in pdf[0:1]:
-
-        # parse layout
-        layout = pdf.parse(page)
-        
-        # extract tables
-        tables = pdf.extract_tables(page)
-        for table in tables:
-            print(table)
-            print()
-        
-        # create docx
-        docx.make_page(layout)
-
-
-    docx.save(docx_file)
-    pdf.close()
+    # extract tables
+    tables = extract_tables(pdf_file, start=0, end=1)
+    for table in tables:
+        print(table)
