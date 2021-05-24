2c00048f5558629579d95daec7db9f07d923d82f
JoHnTsIm <84143748+JoHnTsIm@users.noreply.github.com>
2021-05-24 21:24:58 +0300

    Changes and Improvments
    1. I managed to do your suggestion. Now, the file or files are stored with the same name as the pdf was.
    
    2. When the convert its complete, a window with the message Convert Successful pops up.
    
    3. I remove the Quit Button, but i add a window after the press of X button, that says if you are sure you want quit, if you press OK, the program stops without problem and if you press Cancel the program continues running
    
    4. I made the window smaller insent of 500x700 now its 500x600
    
    5. all the pop up windows working normally.
    
    Knowing Issues:
    1. If there there is a file in this folder, that the user has choose for converted files save location, with the same name as the converted file its gonna be replaced with the converted file.

--- pdf2docx/gui/gui.py
+++ pdf2docx/gui/gui.py
@@ -4,10 +4,13 @@ from tkinter.filedialog import *
 from tkinter import filedialog
 import sys
 import webbrowser
+import os
+from tkinter import messagebox
 
+# Main Window
 root = Tk()
 root.title('PDF_2_Docx Converter')
-root.geometry('500x700')
+root.geometry('500x600')
 root.config(bg='grey')
 root.iconbitmap('icon.ico')
 
@@ -15,10 +18,14 @@ root.iconbitmap('icon.ico')
 # Opens file explorer
 # and let you select choose the pdf file that you want to convert
 def pdf_file_location():
-    global filenames
+    global file_paths, file_names
+    file_names = []
     Tk().withdraw()
-    filenames = askopenfilenames()
-    file_path_pdf_entry.insert(0, filenames)
+    file_paths = askopenfilenames(filetypes=[('PDF file', '*.pdf')])
+    for files in file_paths:
+        file_names.append(os.path.basename(str(files)))
+    print(file_names)
+    file_path_pdf_entry.insert(0, file_paths)
 
 
 # Opens file explorer
@@ -34,46 +41,35 @@ def docx_folder_location():
 # Starts the convert of the file or files
 def convert_button_function():
 
-    # function for pop window 1
+    # function for pop up window 1
     def close():
         root2.destroy()
 
-    # function for pop window 2
+    # function for pop up window 2
     def close_2():
         root3.destroy()
 
-    # function for pop window 3
+    # function for pop up window 3
     def close_3():
         root4.destroy()
 
-    # function for pop window 4
-    def close_4():
-        root5.destroy()
-
-    # function for pop window 5
-    def close_5():
-        root5.destroy()
-        file_number = 0
-        for file in filenames:
-            file_number += 1
-            cv = Converter(file)
-            cv.convert(folder_selected + '/' + 'Docx' + str(file_number) + '.docx', start=0, end=None)
-            cv.close()
-
-    # Window pop ups #
     if len(file_path_docx_entry.get()) == 0 and len(file_path_pdf_entry.get()) == 0:
+
+        # pop up window 3
         root4 = Tk()
         root4.title('Neither files or folder selected')
         root4.geometry('400x100')
         root4.config(bg='grey')
         root4.iconbitmap('icon.ico')
 
+        # Warning Label for pop up window 3
         """Labels"""
         selected_folder = Label(root4, text='Select PDF file or files for convert \n'
                                             'and Select a folder for the converted files!',
                                 font='Arial 15', bg='grey', border=2)
         selected_folder.grid(row=1, column=1, sticky='w', padx=15)
 
+        # Close Button for pop up window 3
         """Buttons"""
         close_button_3 = Button(root4, text='Close', command=close_3)
         close_button_3.grid(column=1, row=2, sticky='n', pady=5, ipady=5, ipadx=5, padx=15)
@@ -81,6 +77,8 @@ def convert_button_function():
         root4.mainloop()
 
     elif len(file_path_pdf_entry.get()) == 0:
+
+        # pop up window 2
         root3 = Tk()
         root3.title('Not files for convert selected')
         root3.geometry('400x80')
@@ -88,10 +86,14 @@ def convert_button_function():
         root3.iconbitmap('icon.ico')
 
         """Buttons"""
+
+        # Close Button for pop up window 2
         close_button_2 = Button(root3, text='Close', command=close_2)
         close_button_2.grid(column=1, row=2, sticky='n', pady=5, ipady=5, ipadx=5, padx=30)
 
         """Labels"""
+
+        # Warning Label for pop up window 2
         selected_files = Label(root3, text='Select PDF file or PDF files for convert', font='Arial 15', bg='grey',
                                border=2)
         selected_files.grid(row=1, column=1, sticky='n', padx=30)
@@ -99,6 +101,8 @@ def convert_button_function():
         root3.mainloop()
 
     elif len(file_path_docx_entry.get()) == 0:
+
+        # pop up window 1
         root2 = Tk()
         root2.title('Not files folder selected')
         root2.geometry('400x80')
@@ -107,10 +111,13 @@ def convert_button_function():
 
         """Buttons"""
 
+        # Close Button for pop up window 1
         close_button = Button(root2, text='Close', command=close)
         close_button.grid(column=1, row=2, sticky='n', pady=5, ipady=5, ipadx=5)
 
         """Labels"""
+
+        # Warning message for pop up window 1
         selected_folder = Label(root2, text='Select a folder for the converted files!', font='Arial 15', bg='grey',
                                 border=2)
         selected_folder.grid(row=1, column=1, sticky='w', padx=30)
@@ -118,33 +125,42 @@ def convert_button_function():
         root2.mainloop()
 
     else:
+        for file in file_paths:
+            names = file_names.pop(0)
+            cv = Converter(file)
+            # cv = Converter(file)
+            cv.convert(folder_selected + '/' + str(names.removesuffix('.pdf')) + '.docx', start=0, end=None)
+            cv.close()
 
-        root5 = Tk()
-        root5.iconbitmap('icon.ico')
-        root5.title('Important!')
-        root5.config(bg='grey')
+        # pop up window 5
+        root6 = Tk()
+        root6.geometry('300x100')
+        root6.config(bg='grey')
+        root6.title('Convert Done!')
+        root6.iconbitmap('icon.ico')
 
-        """Label"""
-        warning_message_label = Label(root5, text='Make sure, that you don"t have any .docx file \n '
-                                                  'in the selected folder with name Docx1, 2, 3, 4..., \n '
-                                                  'because, maybe it will be replaced', font='Impact 20', bg='grey')
-        warning_message_label.grid(column=1, row=1)
+        # function for pop up window 5
+        def close_6():
+            root6.destroy()
+
+        """Labels"""
+
+        # Warning Label for pop up window 5
+        convert_ended = Label(root6, text='Convert Successful', font='Impact 25', bg='grey')
+        convert_ended.grid(padx=15, pady=5, column=1, row=1)
 
         """Buttons"""
 
-        close_button_4 = Button(root5, text='Continue', command=close_5, border=2, font='Arial 10')
-        close_button_4.grid(column=1, row=2, sticky='se', padx=180, ipady=5, ipadx=10)
+        # Close Button for pop up window 5
+        close_button5 = Button(root6, text='Close', font='Arial 15', command=close_6)
+        close_button5.grid(column=1, row=2, padx=30, sticky='n')
 
-        cancel_button = Button(root5, text='Cancel', command=close_4, border=2, font='Arial 10')
-        cancel_button.grid(column=1, row=2, sticky='sw', padx=180, ipady=5, ipadx=10)
-
-        root5.mainloop()
-    ##################
+        root6.mainloop()
 
 
-# Close the program window
-def close_window():
-    sys.exit()
+def on_closing():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        sys.exit()
 
 
 # take you to a webpage, to tell you, how to check your file or files
@@ -155,9 +171,11 @@ def callback():
 
 """Labels"""
 
+# PDF to Docx Label
 program_use_label = Label(text='PDF to Docx', font='Impact 40', bg='white', fg='#1E90FF')
 program_use_label.grid(column=2, row=1, sticky='n', pady=50, padx=120)
 
+# *This converter can only convert text based pdf cant convert image based pdf Label
 check_label = Label(text='*This converter can only convert text based pdf \n cant convert image based pdf',
                     bg='grey')
 check_label.grid(row=7, column=2, pady=10)
@@ -176,22 +194,26 @@ file_path_docx_entry.grid(column=2, ipady=4, ipadx=90, padx=20, sticky='nw', pad
 
 """Buttons"""
 
+# Convert Button
 converter_button = Button(text='Convert', bg='#1E90FF', fg='white', font='impact 20', border=5,
                           command=convert_button_function)
 converter_button.grid(padx=175, sticky='s', ipady=5, ipadx=10, column=2, row=4)
 
+# Select PDF files Button
 select_pdf_file = Button(text='Select PDF files', fg='black', bg='white', border=3,
                          command=pdf_file_location)
 select_pdf_file.grid(column=2, sticky='ne', row=2, pady=6, padx=60)
 
+# Select new files folder Button
 select_new_file_folder = Button(text='Select new files folder', fg='black', bg='white', border=3,
                                 command=docx_folder_location)
 select_new_file_folder.grid(column=2, sticky='ne', row=3, pady=74, padx=26)
 
-quit_button = Button(text='Quit', bg='#1E90FF', fg='white', font='impact 20', border=5, command=close_window)
-quit_button.grid(row=6, column=2, pady=30, ipadx=30)
-
+# how to check my file Button
 file_check_button = Button(text='how to check my file?', bg='grey', fg='black', command=callback)
 file_check_button.grid(column=2)
+
+# Quit window
+root.protocol("WM_DELETE_WINDOW", on_closing)
 
 root.mainloop()
