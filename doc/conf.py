348b37f080b878e399a9c113441ec77e4b51b7c3
Jamie Lemon <jamie.lemon@artifex.com>
2023-12-29 13:27:21 +0000

    Removes napolean from docs dependency.

--- doc/conf.py
+++ doc/conf.py
@@ -40,8 +40,7 @@ release = get_version('../version.txt')
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
