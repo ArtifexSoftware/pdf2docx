fd52953a713509c6107e58f610200768aaf920d9
dothinking <train8808@gmail.com>
2024-01-18 18:33:26 +0800

    optimize class structure

--- pdf2docx/table/Cell.py
+++ pdf2docx/table/Cell.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-
-'''Table Cell object.
-'''
+'''Table Cell object.'''
 
 from docx.shared import Pt
 from ..common.Element import Element
@@ -9,26 +6,25 @@ from ..layout.Layout import Layout
 from ..common import docx
 
 
-class Cell(Element, Layout):
+class Cell(Layout):
     '''Cell object.'''
     def __init__(self, raw:dict=None):
         raw = raw or {}
-        Element.__init__(self, raw=raw)
+        super().__init__()
+        self.restore(raw) # restore blocks and shapes
+
+        # more cell properties
         self.bg_color     = raw.get('bg_color', None) # type: int
         self.border_color = raw.get('border_color', (0,0,0,0)) # type: tuple [int]
         self.border_width = raw.get('border_width', (0,0,0,0)) # type: tuple [float]
         self.merged_cells = raw.get('merged_cells', (1,1)) # type: tuple [int]
-
-        # restore layout
-        Layout.__init__(self, blocks=None, shapes=None) # init empty layout
-        self.restore(raw)
 
 
     @property
     def text(self):
         '''Text contained in this cell.'''
         if not self: return None
-        # NOTE: sub-table may exists in 
+        # NOTE: sub-table may exists in
         return '\n'.join([block.text if block.is_text_block else '<NEST TABLE>'
                                  for block in self.blocks])
 
@@ -43,36 +39,33 @@ class Cell(Element, Layout):
 
 
     def store(self):
-        if bool(self):
-            res = super().store() # Element
-            res.update({
-                'bg_color': self.bg_color,
-                'border_color': self.border_color,
-                'border_width': self.border_width,
-                'merged_cells': self.merged_cells
-            })
-            res.update(Layout.store(self))
-            return res
-        else:
-            return None
+        if not bool(self): return None
+        res = super().store()
+        res.update({
+            'bg_color': self.bg_color,
+            'border_color': self.border_color,
+            'border_width': self.border_width,
+            'merged_cells': self.merged_cells
+        })
+        return res
 
 
     def plot(self, page):
-        '''Plot cell and its sub-layout.'''        
+        '''Plot cell and its sub-layout.'''
         super().plot(page)
         self.blocks.plot(page)
 
 
     def make_docx(self, table, indexes):
         '''Set cell style and assign contents.
-        
+
         Args:
             table (Table): ``python-docx`` table instance.
             indexes (tuple): Row and column indexes, ``(i, j)``.
-        '''        
+        '''
         # set cell style, e.g. border, shading, cell width
         self._set_style(table, indexes)
-        
+
         # ignore merged cells
         if not bool(self):  return
 
@@ -83,7 +76,7 @@ class Cell(Element, Layout):
         if n_row*n_col!=1:
             _cell = table.cell(i+n_row-1, j+n_col-1)
             docx_cell.merge(_cell)
-        
+
         # ---------------------
         # cell width (cell height is set by row height)
         # ---------------------
@@ -92,18 +85,19 @@ class Cell(Element, Layout):
         docx_cell.width = Pt(x1-x0)
 
         # insert contents
-        # NOTE: there exists an empty paragraph already in each cell, which should be deleted first to
-        # avoid unexpected layout. `docx_cell._element.clear_content()` works here.
-        # But, docx requires at least one paragraph in each cell, otherwise resulting in a repair error. 
+        # NOTE: there exists an empty paragraph already in each cell, which should be deleted
+        # first to avoid unexpected layout. `docx_cell._element.clear_content()` works here.
+        # But, docx requires at least one paragraph in each cell, otherwise resulting in a
+        # repair error.
         if self.blocks:
             docx_cell._element.clear_content()
             self.blocks.make_docx(docx_cell)
 
 
     def _set_style(self, table, indexes):
-        '''Set ``python-docx`` cell style, e.g. border, shading, width, row height, 
+        '''Set ``python-docx`` cell style, e.g. border, shading, width, row height,
         based on cell block parsed from PDF.
-        
+
         Args:
             table (Table): ``python-docx`` table object.
             indexes (tuple): ``(i, j)`` index of current cell in table.
@@ -115,7 +109,7 @@ class Cell(Element, Layout):
         # ---------------------
         # border style
         # ---------------------
-        # NOTE: border width is specified in eighths of a point, with a minimum value of 
+        # NOTE: border width is specified in eighths of a point, with a minimum value of
         # two (1/4 of a point) and a maximum value of 96 (twelve points)
         keys = ('top', 'end', 'bottom', 'start')
         kwargs = {}
@@ -128,22 +122,22 @@ class Cell(Element, Layout):
                 'sz': 8*w, 'val': 'single', 'color': hex_c.upper()
             }
 
-        # merged cells are assumed to have same borders with the main cell        
+        # merged cells are assumed to have same borders with the main cell
         for m in range(i, i+n_row):
             for n in range(j, j+n_col):
-                docx.set_cell_border(table.cell(m, n), **kwargs)        
+                docx.set_cell_border(table.cell(m, n), **kwargs)
 
         # ---------------------
         # cell bg-color
         # ---------------------
-        if self.bg_color!=None:
+        if self.bg_color is not None:
             docx.set_cell_shading(docx_cell, self.bg_color)
-        
+
         # ---------------------
         # clear cell margin
         # ---------------------
-        # NOTE: the start position of a table is based on text in cell, rather than left border of table. 
-        # They're almost aligned if left-margin of cell is zero.
+        # NOTE: the start position of a table is based on text in cell, rather than
+        # left border of table. They're almost aligned if left-margin of cell is zero.
         docx.set_cell_margins(docx_cell, start=0, end=0)
 
         # set vertical direction if contained text blocks are in vertical direction
