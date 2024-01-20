fae9965645182fa93fe496a617d4a26a746e42de
dothinking <train8808@gmail.com>
2024-01-20 15:11:30 +0800

    delete gui icon #222

--- pdf2docx/gui/App.py
+++ pdf2docx/gui/App.py
@@ -1,22 +1,18 @@
 '''Main window for ``pdf2docx`` graphic user interface.'''
 
-import os
 import sys
 from tkinter import Tk, messagebox
 from .MainFrame import MainFrame
 
 
 class App(Tk):
+    '''Simple graphic user interface.'''
     def __init__(self, title:str='App', width:int=300, height:int=200):
         '''Top app window.'''
         super().__init__()
         self.title(title)
         self.geometry(f'{width}x{height}')
         self.resizable(0, 0) # not allowed to change size
-
-        # icon
-        icon_path = os.path.join(os.path.dirname(__file__), 'icon.ico') 
-        self.iconbitmap(icon_path)
 
         # layout on the root window
         self.__create_widgets()
